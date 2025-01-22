30a08da884b3977ba3f2f357e36eae5c6d342b15
BuildTools <unconfigured@null.spigotmc.org>
2025-01-22 17:04:45 +0000

    Add project files.

---
+++ NotesApp/Data/NotesContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using NotesApp.Models;
+
+public class NotesContext : DbContext
+{
+    public NotesContext(DbContextOptions<NotesContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Note> Notes { get; set; } // Represents the Notes table
+}
---
+++ NotesApp/Models/Note.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NotesApp.Models
+{
+    public class Note
+    {
+        public int Id { get; set; } // Unique identifier (primary key)
+        int size = 30; //previewText length
+        [Required] // Title is required
+        [StringLength(50)] // Limit the title to 100 characters
+        public string Title { get; set; }
+
+        [Required] // Content is required
+        public string Content { get; set; }
+
+        public string PreviewText
+        {
+            get{
+                if (Content.Length > size)
+                {
+                    return Content.Substring(0,size) + "...";
+                }
+                else 
+                { 
+                    return Content; 
+                }
+            }
+        }
+    }
+}
